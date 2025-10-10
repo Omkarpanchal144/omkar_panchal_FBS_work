49689e962770b279c855cf63cf4c38a6a531ba57
Omkarpanchal144 <omkarpanchal144@gmail.com>
2025-10-10 19:57:54 +0530

    Add files via upload

---
+++ core python/Test/Test_1/question_3.py
@@ -0,0 +1,10 @@
+# convert km in maters and centimeters 
+
+distance=float(input("Enter the distance in km = "))
+
+#calulate the meters and centimeters both
+meter=distance*1000
+centimeters=distance*100000
+print("disttance of mater =",meter,"/M")
+print("disttance  of centimeteri =",centimeters,"/Cm")
+
