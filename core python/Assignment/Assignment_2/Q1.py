d48711e01669ab6c875a1ec8d12430cc4329c5f3
Omkarpanchal144 <omkarpanchal144@gmail.com>
2025-11-02 15:15:01 +0530

    Delete core python/Assignment/Assignment_2 directory

--- core python/Assignment/Assignment_2/Q1.py
+++
@@ -1,8 +0,0 @@
-hours=int(input("Enter the hours = "))
-minutes=int(input("Enter the minutes = "))
-seconds=int(input("Enter the seconds = "))
-
-#convert the total seconds
-total_seconds=(hours*3600)+(minutes*60)+seconds
-
-print("Total seconds =",total_seconds)
--- core python/Assignment/Assignment_2/Q10.py
+++
@@ -1,5 +0,0 @@
-number = int(input(" Enter the three  digit number ="))
-
-rev =(number%10)*100+((number//10)%10)*10+(number//100)
-
-print("Reversed number =",rev)
